07a5d7ddb09e739aaf9347bba95e8340a8365462
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-05-07 21:39:54 +0600

    uppercase

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -1,6 +1,6 @@
 <template>
   <q-page class="flex flex-center">
-    <div class="text-h2 text-bold text-white">Comming Soon...</div>
+    <div class="text-h2 text-bold text-white text-uppercase">Comming Soon...</div>
   </q-page>
 </template>
 
