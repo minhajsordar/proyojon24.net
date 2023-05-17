78238983d5fd4351d587240e6cf8e370fd44a7a2
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-05-17 21:27:17 +0600

    popup added1

--- frontend/src/components/dialogs/profileLayoutAllDialogs.vue
+++ frontend/src/components/dialogs/profileLayoutAllDialogs.vue
@@ -2,16 +2,16 @@
   <div>
     <createDivisionDialog/>
     <createDistrictDialog/>
-    <createSubDistrictDialog/>
+    <!-- <createSubDistrictDialog/>
     <createUnionDialog/>
-    <createWardDialog/>
+    <createWardDialog/> -->
   </div>
 </template>
 <script setup>
 import createDivisionDialog from 'src/components/dialogs/division/createDivisionDialog.vue'
 import createDistrictDialog from 'src/components/dialogs/district/createDistrictDialog.vue';
-import createSubDistrictDialog from 'src/components/dialogs/subdistrict/createSubDistrictDialog.vue';
-import createUnionDialog from 'src/components/dialogs/union/createUnionDialog.vue';
-import createWardDialog from 'src/components/dialogs/ward/createWardDialog.vue';
+// import createSubDistrictDialog from 'src/components/dialogs/subdistrict/createSubDistrictDialog.vue';
+// import createUnionDialog from 'src/components/dialogs/union/createUnionDialog.vue';
+// import createWardDialog from 'src/components/dialogs/ward/createWardDialog.vue';
 
 </script>
