5e5058679b3ee7cafc97e3d25c6bad81710c9feb
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-06-06 20:43:12 +0600

    create edit update added

--- frontend/src/pages/IndexPage.vue
+++ frontend/src/pages/IndexPage.vue
@@ -50,45 +50,51 @@
       </div>
     </div>
   </div>
-  <div class="q-py-md">
-    <div class="row q-col-gutter-sm">
-      <div
-        class="col-lg-3 col-md-3 col-sm-2 col-3"
-        v-for="(service, index) in servicePublicStore.allServices"
-        :key="index"
-      >
-        <div
-          class="text-center cursor-pointer service-item"
-          @click="
-            $router.push('/service_categorys_list/' + service._id);
-            selectedServiceAndCategory.serviceId = service._id;
-            selectedServiceAndCategory.serviceName = service.name;
-          "
-        >
-          <div class="icon-relative-cont">
-            <!-- <q-img  src="/images/hexagonalshape.svg"
+  <div class="container-section-py-sm">
+    <div class="inner-section">
+      <div class="full-width">
+        <div class="q-py-md">
+          <div class="row q-col-gutter-sm">
+            <div
+              class="col-lg-3 col-md-3 col-sm-2 col-3"
+              v-for="(service, index) in servicePublicStore.allServices"
+              :key="index"
+            >
+              <div
+                class="text-center cursor-pointer service-item"
+                @click="
+                  $router.push('/service_categorys_list/' + service._id);
+                  selectedServiceAndCategory.serviceId = service._id;
+                  selectedServiceAndCategory.serviceName = service.name;
+                "
+              >
+                <div class="icon-relative-cont">
+                  <!-- <q-img  src="/images/hexagonalshape.svg"
                   style="width: 56px; height: 48px;"
                   /> -->
-            <q-img src="/images/roundedsquareshape.svg" class="shape" />
-            <q-img
-              class="absolute-top-center service-icon"
-              v-if="service.icon"
-              fit
-              :src="service.icon"
-            />
-            <q-img
-              v-else
-              class="absolute-top-center service-icon"
-              fit
-              src="images/placeholder_image.png"
-            />
-            <!-- <q-img fit
+                  <q-img src="/images/roundedsquareshape.svg" class="shape" />
+                  <q-img
+                    class="absolute-top-center service-icon"
+                    v-if="service.icon"
+                    fit
+                    :src="service.icon"
+                  />
+                  <q-img
+                    v-else
+                    class="absolute-top-center service-icon"
+                    fit
+                    src="images/placeholder_image.png"
+                  />
+                  <!-- <q-img fit
                   style="max-width: 50px; max-height: 50px;"
                   src="https://cdn.pixabay.com/photo/2019/11/06/05/47/stethoscope-4605241_1280.png"
                   /> -->
-          </div>
-          <div>
-            {{ service.name[languageStore.language] }}
+                </div>
+                <div>
+                  {{ service.name[languageStore.language] }}
+                </div>
+              </div>
+            </div>
           </div>
         </div>
       </div>
@@ -261,23 +267,23 @@ useMeta(metaData);
   height: 40px;
 }
 
-
-.service-item{
+.service-item {
   /* min-width:78px; */
   /* height: 50px; */
   display: flex;
-    flex-direction: column;
-    align-items: center;
-    padding-bottom: 8px;
-font-size: 14px;
-box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 2px 2px rgba(0, 0, 0, 0.14), 0 3px 1px -2px rgba(0, 0, 0, 0.12);
-border-radius: 4px;
-vertical-align: top;
-background: #fff;
-position: relative;
+  flex-direction: column;
+  align-items: center;
+  padding-bottom: 8px;
+  font-size: 14px;
+  box-shadow: 0 1px 5px rgba(0, 0, 0, 0.2), 0 2px 2px rgba(0, 0, 0, 0.14),
+    0 3px 1px -2px rgba(0, 0, 0, 0.12);
+  border-radius: 4px;
+  vertical-align: top;
+  background: #fff;
+  position: relative;
 }
 @media screen and (max-width: 950px) {
-  .service-item{
+  .service-item {
     padding-bottom: 4px;
     font-size: 11px;
     box-shadow: none;
@@ -285,53 +291,52 @@ position: relative;
     vertical-align: top;
     background: transparent;
     position: relative;
+  }
 }
+.margin-neg {
+  margin: -4px;
 }
-.margin-neg{
-  margin:-4px;
-}
-.icon-relative-cont{
+.icon-relative-cont {
   position: relative;
   width: 120px;
   height: 100px;
 }
-.absolute-top-center{
+.absolute-top-center {
   position: absolute;
-    left: 50%;
-    top: 50%;
-    transform: translate(-50%,-50%);
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 }
-.shape{
+.shape {
   width: 98px;
   height: 98px;
 }
-.service-icon{
+.service-icon {
   max-width: 70px;
   max-height: 70px;
 }
 @media screen and (max-width: 950px) {
-
-.margin-neg{
-  margin:-4px;
-}
-.icon-relative-cont{
-  position: relative;
-  width: 60px;
-  height: 50px;
-}
-.absolute-top-center{
-  position: absolute;
+  .margin-neg {
+    margin: -4px;
+  }
+  .icon-relative-cont {
+    position: relative;
+    width: 60px;
+    height: 50px;
+  }
+  .absolute-top-center {
+    position: absolute;
     left: 50%;
     top: 50%;
-    transform: translate(-50%,-50%);
-}
-.shape{
-  width: 48px;
-  height: 48px;
-}
-.service-icon{
-  max-width: 35px;
-  max-height: 35px;
-}
+    transform: translate(-50%, -50%);
+  }
+  .shape {
+    width: 48px;
+    height: 48px;
+  }
+  .service-icon {
+    max-width: 35px;
+    max-height: 35px;
+  }
 }
 </style>
