c4f7d4c8dbbac971f8894105935edb8057cbae14
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-06-20 02:21:41 +0600

    create edit update add

--- backend/server.js
+++ backend/server.js
@@ -38,9 +38,9 @@ dotenv.config()
 const app = express()
 const httpServer = createServer(app);
 const io = new Server(httpServer, {
-    cors: {
-        origin: "http://localhost:9000"
-    },
+    // cors: {
+    //     origin: "http://localhost:9000"
+    // },
     allowRequest: (req, callback) => {
         callback(null, req.headers.origin === undefined); // cross-origin requests will not be allowed
       }
