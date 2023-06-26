98d418fad1f4141a79bc763a9aee7b7f70b01915
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-06-26 22:10:10 +0600

    frontend edit option add

--- backend/models/serviceProviderModel.js
+++ backend/models/serviceProviderModel.js
@@ -1,5 +1,5 @@
 import mongoose from "mongoose";
-var CounterSchema = Schema({
+var CounterSchema = new mongoose.Schema({
     _id: {type: String, required: true},
     seq: { type: Number, default: 0 }
 });
