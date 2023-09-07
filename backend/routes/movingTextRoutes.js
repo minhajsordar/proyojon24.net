92afab991f6b45cf6bdde54e5001bdd3601e7a56
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-09-07 14:02:33 +0600

    delete file remove

--- backend/routes/movingTextRoutes.js
+++ backend/routes/movingTextRoutes.js
@@ -2,15 +2,13 @@ import express from "express";
 import {    getMovingText,
     getMovingTextById,
     updateMovingText,
-    createMovingText,
-    removeMovingText
+    createMovingText
 } from '../controllers/movingTextController.js'
     import { protect, superAdmin } from "../middleware/authMiddleware.js"
 const router = express.Router()
 
 router.route('/').get(getMovingText).post(protect, superAdmin, createMovingText)
 
-router.route('/remove').get(removeMovingText)
 router.route('/:id')
     .get(getMovingTextById)
     .put(protect, superAdmin, updateMovingText)
