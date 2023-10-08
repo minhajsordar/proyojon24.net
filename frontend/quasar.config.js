aa3334b00253fe39fb824629b99ed25cb6fbc854
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2023-10-08 06:24:01 +0600

    builder added

--- frontend/quasar.config.js
+++ frontend/quasar.config.js
@@ -76,7 +76,7 @@ module.exports = configure(function (/* ctx */) {
       // minify: false,
       // polyfillModulePreload: true,
       // distDir
-      distDir:"../build",
+      distDir:"../../root@noormohammad-vps/build",
 
       // extendViteConf (viteConf) {},
       // viteVuePluginOptions: {},
